f13c6d83a34dbf75d13c771227f2ff6c19449fa1
ediesol <152651162+ediesol@users.noreply.github.com>
2024-01-12 17:10:56 +0100

    fix bugs, improve design, add pause function
    - partial fix of timer not running in a background tab (still might not ring, but duration is handled correctly)
    - fix input validation issues
    - design improvements
    - add pause function
    - minor code improvements

--- timer.js
+++ timer.js
@@ -1,33 +1,53 @@
-const inputFields = document.querySelectorAll(".timeField");
+"use strict";
+
+let random;
+let rounded;
+let randX;
+let randY;
+
+function applyBackground() {
+    document.body.style.backgroundImage = `url("media/pattern${rounded}.png")`;
+    if (rounded == 0) {
+        document.body.style.backgroundSize = "35vh"
+    } else {
+        document.body.style.backgroundSize = "420vh"
+    };
+    document.body.style.backgroundPositionX = `${randX}px`;
+    document.body.style.backgroundPositionY = `${randY}px`;
+};
+
+(function setBackground() {
+    random = Math.random();
+    rounded = Math.round(random);
+    randX = Math.random() * -1500;
+    randY = Math.random() * -1000;
+    applyBackground();
+})();
+
+//global scope DOM elements
 const startButton = document.querySelector("#start");
+startButton.setAttribute("disabled", "");
 const intervalOption = document.querySelector("#interval");
 const silentOption = document.querySelector("#silent");
 const footer = document.querySelector("footer");
-let inputH;
-let inputM;
-let inputS;
+const pauseButton = document.querySelector("figure button");
 
-startButton.setAttribute("disabled", "");
+//global scope color setting
+let root = document.querySelector(":root");
+root.style.setProperty('--varcolor', 'lightgreen');
 
-//CODE CONTROLLING INPUT ACTION
+(function hideWarning() {
+    const warning = document.querySelector(".warning");
+    warning.addEventListener("click", function hide() {
+        warning.style.display = "none";
+    })
+})();
 
-//checks for valid input and activates start button.
-function validInput(event) {
 
-    empty = 0;
-
-    let newInput = event.target.value;
-    const regex = /^\d*[1-9]+\d*$/;
-    let valid = regex.test(newInput);
-
-    if (valid == true) {
-        startButton.removeAttribute("disabled");
-    } else {
-        startButton.setAttribute("disabled", "");
-    }
-}
+//INPUT ACTIONS
 
 (function optionsExclusion() {
+
     silentOption.addEventListener("click", function(event) {
         intervalOption.checked = false;
     })
@@ -37,166 +57,275 @@ function validInput(event) {
     })
 })();
 
-//event listener for input
+//enables start button on valid input
 (function enableStart() {
-    for (let i = 0; i < inputFields.length; i++) {
-        inputFields[i].addEventListener("input", validInput);
+
+    let validField = 0;
+    let activeTarget;
+    const inputFields = document.querySelectorAll(".timeField");
+
+    function validInput(event) {
+
+        if (inputFields[0].value > 24) {
+            alert("Duration cannot exceed 24 hours. Sorry!");
+            inputFields[0].value = "";
+        }
+
+        let completeInput = "";
+        for (let inputField of inputFields) {
+            completeInput = completeInput + inputField.value;
+        }
+
+        const regex = /^\d*[1-9]+\d*$/;
+        let valid = regex.test(completeInput);
+
+        if (valid == true) {
+            startButton.removeAttribute("disabled");
+        } else {
+            startButton.setAttribute("disabled", "");
+        }
     }
+
+    for (let inputField of inputFields) {
+        inputField.addEventListener("input", validInput);
+    }
+
 })();
 
 (function detailsHandler() {
     const timerInfo = footer.querySelector("p");
     const navButton = footer.querySelector("nav");
     let isOpen = false;
-    footer.addEventListener("click", function handleDetails() {
-        if (isOpen == false) {
-            timerInfo.style.display = "inline-block";
+
+    document.addEventListener("click", function handleDetails(e) {
+        if (e.target == navButton && isOpen == false) {
             navButton.innerHTML = "close";
             isOpen = true;
-            navButton.style.color = "lightgreen";
+            timerInfo.classList.add('footer-opened');
+            navButton.classList.add('nav-opened');
+
         } else if (isOpen == true) {
-            timerInfo.style.display = "none";
             navButton.innerHTML = "details";
             isOpen = false;
-            navButton.style.color = "black";
+            timerInfo.classList.remove('footer-opened');
+            navButton.classList.remove('nav-opened');
         }
     })
 
 })();
 
 
-//CODE CONTROLLING COUNTDOWN ACTION
+//COUNTDOWN ACTIONS
 (function buttonTrigger() {
     startButton.addEventListener("click", function timerStart(event) {
         event.preventDefault();
+        const startTime = new Date().getTime();
         const countdownDisplay = document.querySelector("figcaption");
         const blocks = document.querySelectorAll("progress");
         const timerDisplay = document.querySelector("figure");
         const endBox = document.querySelector("#endMessage");
         const inputBox = document.querySelector("main");
-        inputH = document.querySelector("#hours").value;
-        inputM = document.querySelector("#minutes").value;
-        inputS = document.querySelector("#seconds").value;
-        const startTimeMS =
-            1000 * inputS + 1000 * 60 * inputM + 1000 * 60 * 60 * inputH;
+        let inputTimes = [];
+        inputTimes.push(document.querySelector("#hours").value);
+        inputTimes.push(document.querySelector("#minutes").value);
+        inputTimes.push(document.querySelector("#seconds").value);
+        let durationMS = 0;
+        let paused = false;
 
-        //this initializes the timer
+        function timeDisplay(a, b, c) {
+            let timeUnits = ["hour", "minute", "second"];
+            let timeValues = [a, b, c];
+
+            for (let i = 0; i < timeValues.length; i++) {
+
+                if (timeValues[i] > 1) {
+                    timeUnits[i] = timeUnits[i] + "s";
+                } else if (timeValues[i] == 0) {
+                    timeUnits[i] = "";
+                    timeValues[i] = "";
+                }
+
+            }
+
+            countdownDisplay.innerHTML = `${timeValues[0]} ${timeUnits[0]} ${timeValues[1]} ${timeUnits[1]} ${timeValues[2]} ${timeUnits[2]}`
+
+        }
+
+        (function setUpTimer() {
+
+            //formats input into proper time format
+            for (let i = 0; i < inputTimes.length; i++) {
+
+                if (inputTimes[i] == false) {
+                    inputTimes[i] = 0;
+                }
+
+                function calcTime() {
+                    if (inputTimes[i] > 59 && i > 0) {
+                        inputTimes[i] = inputTimes[i] - 60;
+                        inputTimes[i - 1]++;
+                        calcTime();
+                    }
+                }
+                calcTime();
+            }
+
+            timeDisplay(inputTimes[0], inputTimes[1], inputTimes[2]);
+
+            timerDisplay.style.display = "flex";
+            timerDisplay.style.opacity = "100";
+            pauseButton.style.display = "inline-block";
+            for (let block of blocks) {
+                block.style.display = "inline-block";
+            };
+
+
+        })();
+
+        (function timeInMillisec() {
+            let x = 1000;
+
+            for (let i = inputTimes.length - 1; i >= 0; i--) {
+                durationMS = durationMS + inputTimes[i] * x;
+                x = x * 60;
+            }
+            durationMS = durationMS + 100;
+        })();
+
+        let timerEndTime = startTime + durationMS;
+        let remainingTime;
+        let rH;
+        let rM;
+        let rS;
+        let bellCount = blocks.length - 1;
+        let mustStop = false;
 
         (function hideInputBox() {
             inputBox.style.display = "none";
             footer.style.display = "none";
-
+            document.body.style.backgroundImage = 'none';
         })();
 
-        (function setUpTimer() {
-            //this gives empty fields a 0 value
-            if (inputH == false) {
-                inputH = 0;
+        (function rotateBlocks() {
+            let angle = 180;
+            for (let block of blocks) {
+                block.style.transform = `rotate(${angle}deg)`;
+                angle = angle - 90;
             }
-            if (inputM == false) {
-                inputM = 0;
-            }
-            if (inputS == false) {
-                inputS = 0;
-            }
-
-            console.log("setup function running");
-
-            timerDisplay.style.display = "flex";
-            countdownDisplay.innerHTML = `${inputH} hours ${inputM} minutes ${inputS} seconds`;
-
         })();
-
-        (function rotateBlocks() {})();
-        let angle = 180;
-        for (block of blocks) {
-            block.style.transform = `rotate(${angle}deg)`;
-            angle = angle - 90;
-        }
 
         function runTimer() {
+
             //actualizes countdown time
-            (function countDown() {
-                if (inputH > 0) {
-                    if (inputM == 0) {
-                        inputM = 59;
-                        inputH--;
+            (function countdown() {
+                //calculates difference between current time and aimed end time
+                let currentTime = new Date().getTime();
+                remainingTime = timerEndTime - currentTime;
+                rH = Math.floor((remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                rM = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
+                rS = Math.floor((remainingTime % (1000 * 60)) / 1000);
+                timeDisplay(rH, rM, rS);
+
+                (function endDisplay() {
+                    if (remainingTime <= 1000) {
+                        for (let block of blocks) {
+                            block.style.display = "none";
+                        }
+                        countdownDisplay.innerHTML = "time's up!";
+                        countdownDisplay.classList.add("end-message");
+                        pauseButton.style.display = "none";
+
+                        if (!mustStop) {
+                            mustStop = true;
+                            setTimeout(function hideEndBox() {
+                                let hasRun = true;
+                                timerDisplay.style.transition = "all 1s ease-out"
+                                timerDisplay.style.opacity = "0";
+                                setTimeout(function() {
+                                    countdownDisplay.classList.remove("end-message");
+                                    inputBox.style.display = "block";
+                                    footer.style.display = "block";
+                                    applyBackground();
+                                    timerDisplay.style.transition = "none"
+                                }, 1200);
+                            }, 2000)
+                        }
                     }
-                }
 
-                if (inputM > 0) {
-                    if (inputS == 0) {
-                        inputS = 59;
-                        inputM--;
-                    } else {
-                        inputS--;
+                    if (remainingTime <= 0) {
+                        clearInterval(runningInterval);
+                        mustStop = false;
                     }
-                } else {
-                    inputS--;
-                }
 
-                countdownDisplay.innerHTML = `${inputH} hours ${inputM} minutes ${inputS} seconds`;
+                })();
 
-                if (inputS == 0 && inputM == 0 && inputH == 0) {
-                    clearInterval(runningInterval);
-                    endBox.style.display = "block";
-                    setTimeout(function hideEndBox() {
-                        endBox.style.display = "none";
-                        inputBox.style.display = "block";
-                        footer.style.display = "block";
-                    }, 4000)
-                }
             })();
 
             (function progressActions() {
-                let endTimeMS =
-                    1000 * inputS + 1000 * 60 * inputM + 1000 * 60 * 60 * inputH;
-                let amount = (endTimeMS / startTimeMS) * (blocks.length);
-                let bC = Math.ceil(amount) - 1;
+                let amount = (remainingTime / durationMS) * (blocks.length);
+                let fAmount = Math.floor(amount);
+
+                //controls display of the progress blocks
+                function timeBlocks(i) {
+
+                    if (amount <= 0) {
+                        blocks[0].value = 0;
+                        for (let block of blocks) {
+                            block.value = 100;
+                        };
+
+                    } else if (fAmount == i) {
+
+                        let newValue = (amount - fAmount) * 100;
+                        blocks[fAmount].value = newValue;
+
+                        if (fAmount < blocks.length - 1) {
+                            blocks[fAmount + 1].value = 0;
+                        };
+                    }
+                }
 
                 for (let i = blocks.length - 1; i >= 0; i--) {
 
+                    //control bells
                     (function invervalBell() {
-                        //the following variables are used to prevent uneven durations from not ringing 1/4 bells
-                        let oddHalf = ((endTimeMS + 500) / startTimeMS) * (blocks.length);
-                        let oddQuarterMinus = ((endTimeMS + 250) / startTimeMS) * (blocks.length);
-                        let oddQuarterPlus = ((endTimeMS - 250) / startTimeMS) * (blocks.length);
                         let interBell = new Audio(`media/bell${i}.mp3`);
 
-                        if (intervalOption.checked == true && (amount == i || oddHalf == i || oddQuarterMinus == i || oddQuarterPlus == i)) {
+                        if (intervalOption.checked == true && (amount <= i && i <= bellCount)) {
                             interBell.play();
-                        } else if (silentOption.checked == false && amount == i && i == 0) {
+                            bellCount--;
+                        } else if (mustStop && i == 0 && silentOption.checked == false) {
                             interBell.play();
-                        }
-                    })();
-
-                    //this controls the display of the progress blocks
-                    (function timeBlocks() {
-                        if (amount == 0) {
-                            blocks[0].setAttribute("value", 0);
-                            var reset = setTimeout(function reset() {
-                                for (block of blocks) {
-                                    block.value = 100;
-                                };
-                                timerDisplay.style.display = "none";
-                                console.log("timeblock ran");
-                            }, 1000)
-                        } else if (amount > i) {
-                            let newValue = (amount - bC);
-                            if (bC >= 0) {
-                                blocks[bC].setAttribute("value", newValue * 100);
-                            };
-
-                            if (bC < blocks.length - 1) {
-                                blocks[bC + 1].setAttribute("value", 0);
-                            };
                             return;
                         }
                     })();
+
+                    timeBlocks(i);
                 }
             })();
 
         }
+
+        pauseButton.addEventListener("click", function playPause() {
+
+            let restartTime;
+
+            if (paused == false) {
+                clearInterval(runningInterval);
+                paused = true;
+                pauseButton.innerHTML = "▶"
+                pauseButton.classList.add('paused');
+                countdownDisplay.classList.add('paused');
+            } else {
+                restartTime = new Date().getTime();
+                timerEndTime = restartTime + remainingTime;
+                paused = false;
+                pauseButton.innerHTML = "II";
+                pauseButton.classList.remove('paused');
+                countdownDisplay.classList.remove('paused');
+                runningInterval = setInterval(runTimer, 1000);
+            }
+        });
 
         let runningInterval = setInterval(runTimer, 1000);
     });
